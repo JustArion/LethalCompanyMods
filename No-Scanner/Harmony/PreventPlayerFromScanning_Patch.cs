a75030e41582d7d91677889d1886d99d6e87e540
arion <53127594+JustArion@users.noreply.github.com>
2024-07-22 04:22:52 +0200

    Add Ship Exclusionary Config

--- No-Scanner/Harmony/PreventPlayerFromScanning_Patch.cs
+++ No-Scanner/Harmony/PreventPlayerFromScanning_Patch.cs
@@ -1,12 +1,35 @@
 ﻿namespace NoScanner.Harmony;
 
+using System;
 using HarmonyLib;
 using JetBrains.Annotations;
 
 [HarmonyPatch(typeof(HUDManager), "CanPlayerScan")]
 internal static class PreventPlayerFromScanning_Patch
 {
+    private static bool _loggedErrorOnce;
+    private static bool _isInShip => GameNetworkManager.Instance.localPlayerController.isInElevator;
+    
     [UsedImplicitly]
     [HarmonyPrefix]
-    private static bool OnCanPlayerScan(ref bool __result) => __result = false;
+    private static bool OnCanPlayerScan(ref bool __result)
+    {
+        try
+        {
+            if (Plugin.DisableOnlyInShip.Value)
+                return !_isInShip;
+
+            return __result = false;
+        }
+        catch (Exception e)
+        {
+            if (_loggedErrorOnce) 
+                return true;
+            
+            Plugin.ModLogger.LogError(e);
+            _loggedErrorOnce = true;
+            return true;
+        }
+
+    }
 }
--- No-Scanner/Plugin.cs
+++ No-Scanner/Plugin.cs
@@ -2,14 +2,21 @@
 
 using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
 using PluginInfo = Dawn.LC.NoScanner.PluginInfo;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 internal sealed class Plugin : BaseUnityPlugin
 {
+    public static ManualLogSource ModLogger { get; private set; }
+
+    public static ConfigEntry<bool> DisableOnlyInShip;
     private void Awake()
     {
+        ModLogger = Logger;
         HarmonyLib.Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
-        Logger.LogInfo($"Checking in ;)");
+        DisableOnlyInShip = Config.Bind("NoScanner", "DisableOnlyInShip", false, "Disables the use of the Scanner in the ship, everywhere else it's allowed");
+        Logger.LogInfo("Checking in ;)");
     }
 }
